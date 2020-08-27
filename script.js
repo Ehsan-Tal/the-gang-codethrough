7ea88c34b8bf96a6dc7794b725cae7d860a8981a
MehsterTurdlord <65217868+MehsterTurdlord@users.noreply.github.com>
2020-08-27 23:19:04 +0400

    Added Script
    Did things

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function ShowPopup() {
+    let popup = document.getElementById('popup')
+    popup.classList.add('show')
+}
+
+function HidePopup() {
+    let popup = document.getElementById('popup')
+    popup.classList.remove('show')
+}
